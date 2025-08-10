0ff38b204e3ceee40ceacb553a67102adbd9a4da
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2025-08-10 10:42:40 +0200

    Added audio input.

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import streamlit as st
 class App:
     def run(self):
         st.title("Streamlit Whisper")
+        audio_value = st.audio_input("Record a voice message")
+        if audio_value:
+            st.audio(audio_value)
 
 
 if __name__ == "__main__":
