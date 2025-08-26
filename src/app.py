5680107d388ae0a4220ea55d81ccc125c7a91385
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2025-08-26 21:11:30 +0200

    Added sidebar settings.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 from client.client import Client
+from config import Config
 from stt import Whisper
 
 
@@ -8,10 +9,79 @@ class App:
 
     def __init__(self):
         self.client = Client()
+        self.config = Config().get_config()
         self.stt = Whisper()
+
+
+    def _sidebar_settings(self, config):
+        st.sidebar.header("Settings")
+        client = st.sidebar.selectbox(
+            "Client",
+            [
+                "groq",
+                "lmstudio",
+                "ollama",
+                "openai",
+            ],
+            index=[
+                "groq",
+                "lmstudio",
+                "ollama",
+                "openai",
+            ].index(config["client"] or "ollama"),
+        )
+        language = st.sidebar.selectbox(
+            "Language",
+            [
+                "english",
+                "german",
+            ], index=[
+                "english",
+                "german",
+            ].index(config["language"] or "german"),
+        )
+        model = st.sidebar.text_input(
+            "Whisper Modell",
+            value=config["model"] or "base",
+        ),
+        speaker_recognition = st.sidebar.checkbox(
+            "Speaker recognition",
+            value=False,
+        )
+        summary = st.sidebar.checkbox(
+            "Summary generation",
+            value=False,
+        )
+        temperature = st.sidebar.slider(
+            "Temperature",
+            max_value=1.0,
+            min_value=0.0,
+            step=0.05,
+            value=float(config["groq"]["temperature"] or 0.7),
+        )
+        groq_api_key = st.sidebar.text_input(
+            "Groq API Key",
+            type="password",
+            value=config["groq"]["api_key"] or "",
+        )
+        openai_api_key = st.sidebar.text_input(
+            "OpenAI API Key",
+            value=config["openai"]["api_key"] or "",
+        )
+        return {
+            "client": client,
+            "language": language,
+            "model": model,
+            "speaker_recognition": speaker_recognition,
+            "summary": summary,
+            "temperature": temperature,
+            "groq_api_key": groq_api_key,
+            "openai_api_key": openai_api_key,
+        }
 
     def run(self):
         st.title("Streamlit Whisper")
+        st.session_state["settings"] = self._sidebar_settings(self.config)
         audio_tab, upload_tab = st.tabs(["Audio", "Upload"])
         with audio_tab:
             self.audio = st.audio_input("Record audio", label_visibility="hidden")
